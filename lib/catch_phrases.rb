09d8569b5dcda2bc164761d8a8412de733dad0cf
Alfred Wang <alfredwang0707@gmail.com>
2020-09-29 21:48:07 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,3 +9,11 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link 
+  puts "It's Dangerous To Go Alone! Take This."
+end
+
+def all_phrases
+  puts phrase + status + link
+end
